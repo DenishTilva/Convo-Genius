05fa99871a0bb23245225794cd6a49578608c35b
DenishTilva <pateldenish5646@gmail.com>
2024-06-02 19:17:01 +0530

    Conversation-Functionality

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as clerk from "../clerk.js";
+import type * as conversations from "../conversations.js";
 import type * as http from "../http.js";
 import type * as schema from "../schema.js";
 import type * as users from "../users.js";
@@ -29,6 +30,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   clerk: typeof clerk;
+  conversations: typeof conversations;
   http: typeof http;
   schema: typeof schema;
   users: typeof users;
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,6 @@
 import {defineSchema, defineTable} from 'convex/server'
 import { v } from 'convex/values'
-import { text } from 'stream/consumers'
+// import { text } from 'stream/consumers'
 
 export default defineSchema({
     users:defineTable({
@@ -9,5 +9,14 @@ export default defineSchema({
         image:v.string(),
         tokenIdentifier:v.string(),
         isOnline:v.boolean(),
-    }).index("by_tokenIdentifier", ["tokenIdentifier"])
+    }).index("by_tokenIdentifier", ["tokenIdentifier"]),
+
+    conversations:defineTable({
+        participants:v.array(v.id("users")),
+        isGroup:v.boolean(),
+        groupName:v.optional(v.string()),
+        groupImage:v.optional(v.string()),
+        admin:v.optional(v.id("users"))
+    })
+
 })
