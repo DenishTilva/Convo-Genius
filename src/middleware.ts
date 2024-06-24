ed580d2d228368c0a9144fb0d8fa10323d20e57e
Denish Patel <159614328+DenishTilva@users.noreply.github.com>
2024-06-25 00:16:08 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,3 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
-
-// export default authMiddleware({
-//   // ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\\w]+$)", "/"],
-// });
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
@@ -22,10 +13,3 @@ export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 }
 
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
